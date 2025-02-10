214f0514f87ebec8a0fa7344882511ad8235b5b0
Tito Daramola <titodaramola@yahoo.com>
2025-02-10 17:22:23 +0000

    Connect Postgresql database to django

--- thesongexchange/settings.py
+++ thesongexchange/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +80,8 @@ WSGI_APPLICATION = 'thesongexchange.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':
+dj_database_url.parse(os.environ.get('postgresql://neondb_owner:npg_d7XAl6PLYvMc@ep-falling-darkness-a2rb6toq.eu-central-1.aws.neon.tech/arise_feed_donut_277130'))
 }
 
 
