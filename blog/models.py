c382293d0eed634de307b923eeb0cdbf0c5bb46a
Tito Daramola <titodaramola@yahoo.com>
2025-02-13 01:02:19 +0000

    Add posts HTML template boilerplate

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,9 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updates_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+    
+    def __str__(self):
+        return f"{self.title} written by {self.author}"
