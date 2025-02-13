cac807329d9b7f9a1d6472f3fbe75ba9ee1a8ada
Tito Daramola <titodaramola@yahoo.com>
2025-02-13 14:18:42 +0000

    Add new template for viewing full post, add iframe support for song url

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -7,3 +7,13 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/posts.html'
     
+
+def post_detail(request, slug):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/fullpost.html",
+        {"post": post},
+    )
