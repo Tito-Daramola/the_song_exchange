cf595305441a2d0915de8ecd3bb650fe24df2dbb
Tito Daramola <titodaramola@yahoo.com>
2025-02-13 16:02:20 +0000

    Add profile page template

--- my_profile/views.py
+++ my_profile/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Create your views here.
+def profile_page(request, username):
+    user = get_object_or_404(User, username=username)
+    profile = get_object_or_404(Profile, user=user)
+    posts = profile.get_user_posts()
+    return render(request, 'my_profile/profile_page.html', {'profile': profile, 'posts': posts})
--- thesongexchange/settings.py
+++ thesongexchange/settings.py
@@ -29,7 +29,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = 'django-insecure-^ff#9tqc2!ppiq0ao-y9+^_h1fswtuev7()-*9cjg%l$k#1%($'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
